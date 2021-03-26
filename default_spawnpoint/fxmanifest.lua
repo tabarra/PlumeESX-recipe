008b3e6fd57c17c3bc207d11ea2eb69cdceba747
Andre Tabarra <tabarra@vexguard.net>
2021-03-26 13:13:50 -0300

    added default spawn, updated repos, sv_maxclients

---
+++ default_spawnpoint/fxmanifest.lua
@@ -0,0 +1,9 @@
+fx_version 'cerulean'
+games { 'rdr3', 'gta5' }
+
+author 'PlumeESX'
+description 'Default spawnpoint at the job center.'
+version '1.0.0'
+
+resource_type 'map' { gameTypes = { ['basic-gamemode'] = true } }
+map 'map.lua'
---
+++ default_spawnpoint/map.lua
@@ -0,0 +1,1 @@
+spawnpoint 'a_m_y_hipster_01' { x = -265.0, y = -963.6, z = 30.2 }
